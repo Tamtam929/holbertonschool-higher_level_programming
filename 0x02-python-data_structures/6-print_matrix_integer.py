dba15cf89f141abd386a3d293314dda5fd82051d
TammyJunk <Tamtam929@gmail.com>
2023-01-13 18:29:16 +0000

    5

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,8 @@
-i
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for number in i:
+            print(f"{number}", end="")
+            if number != i[-1]:
+                print(f" ", end="")
+            print()
