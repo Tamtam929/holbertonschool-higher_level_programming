24815d48829f361fbc0d023a413ed24c2fe6509e
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-01-15 12:46:50 -0600

    trying something else

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for count in range(1, 101):
-        if count % 15 == 0:
+        if count % 3 == 0 and x % 5 == 0:
             print("FizzBuzz", end="")
         elif count % 3 == 0:
             print("Fizz ", end="")
