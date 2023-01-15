321bfb93ecc04f016c7c4f95eeca2e2dc246b7b1
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-01-15 12:53:58 -0600

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,8 +4,8 @@ def fizzbuzz():
         if count % 3 == 0 and count % 5 == 0:
             print("FizzBuzz", end=" ")
         elif count % 3 == 0:
-            print("Fizz ", end=" ")
+            print("Fizz", end=" ")
         elif count % 5 == 0:
-            print("Buzz ", end=" ")
+            print("Buzz", end=" ")
         else:
-            print("{:d} ".format(count), end=" ")
+            print("{} ".format(count), end=" ")
