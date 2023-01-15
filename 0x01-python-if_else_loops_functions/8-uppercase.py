4d7904b9eb2d1b7493c8d145c386bf876c54cf13
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-01-15 12:44:17 -0600

    Fixing spacing

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for u in str:
         if ord(u) >= 97 and ord(u) <= 123:
             u = chr(ord(u) - 32)
-print("{}".format(u), end="")
-print("")
+        print("{}".format(u), end="")
+        print("")
