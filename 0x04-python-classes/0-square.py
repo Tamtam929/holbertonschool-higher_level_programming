a72944d2f98212d4de42f7ad1a48285f564f72ab
TammyJunk <Tamtam929@gmail.com>
2023-01-18 03:36:17 +0000

    forgot space

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -6,4 +6,4 @@ class Square:
     """this class defines a square"""
     def __init__(self, size=0):
         """this initializes a simple square"""
-    self.__size = size
+        self.__size = size
