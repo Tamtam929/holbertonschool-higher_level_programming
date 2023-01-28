a4e7794922287e0c97443c529afa669fd16e46fd
TammyJunk <Tamtam929@gmail.com>
2023-01-28 16:17:34 +0000

    2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,10 @@
-i
+#!/usr/bin/python3
+"""function that appends a string at the end of a text file"""
+
+
+def append_write(filename="", text=""):
+    """function that appends a string at the end of a text file (UTF8) and
+returns the number of characters added"""
+    with open(filename, "a", encoding="utf-8") as f:
+        appended = f.write(text)
+    return (appended)
