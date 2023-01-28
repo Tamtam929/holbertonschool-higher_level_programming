1a6bb4e76acf5c0473433e5e82cae80ee8c7667a
TammyJunk <Tamtam929@gmail.com>
2023-01-28 17:02:12 +0000

    8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,7 @@
-1
+#!/usr/bin/python3
+"""Defines a function that returns dictionary description of an object."""
+
+
+def class_to_json(obj):
+    """Returns dictionary description for JSON serilization of an object."""
+    return vars(obj)
