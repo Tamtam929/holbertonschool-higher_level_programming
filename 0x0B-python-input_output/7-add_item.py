663494de2147fa45fc88a838d930f937b11c443d
Tammy <5554@holbertonstudents.com>
2023-02-01 02:11:32 +0000

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,16 +2,15 @@
 """Load, add, save"""
 
 import sys
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
 
-try:
-    i = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    i = []
-
-for a in sys.argv[1:]:
-    i.append(a)
-
-save_to_json_file(i, "add_item.json")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
