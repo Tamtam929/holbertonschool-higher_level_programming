ef1d8e52cfba56ca512824fb8f332d7303eb5cd2
Tamtam929 <tamtam929@gmail.com>
2023-02-13 10:36:33 -0800

    12

--- 0x00-SQL_introduction/11-best_score.sql
+++ 0x00-SQL_introduction/11-best_score.sql
@@ -1,2 +1,3 @@
--- Lists all records with a score >= 10 in the table second_table
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;01~
+-- List all records 'score >= 10' in table 'second_table'
+SELECT score, name FROM second_table WHERE score >= 10
+ORDER BY score DESC;
