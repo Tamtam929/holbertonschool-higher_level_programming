297e7d7937b2802785a2c6827e3df746b5ab80bc
Tamtam929 <tamtam929@gmail.com>
2023-03-01 08:22:37 -0800

    10

--- 0x0B-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,24 +1,38 @@
 #!/usr/bin/python3
-"""script that lists all State objects that contain the letter a
-from the database hbtn_0e_6_usa"""
+"""
+Script that prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
 
 if __name__ == "__main__":
-    """Access to db and get the values"""
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    from sys import
+    # Get the arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    # Engine creation
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(argv[1], argv[2], argv[3]))
-
-    # Session initializer (automatically commits and closes sessions)
-    Session = sessionmaker(engine)
+    # Create the engine and the session
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            mysql_username, mysql_password, database_name
+        ),
+        pool_pre_ping=True,
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Obtaining Query Results
-    for result in session.query(State).filter(State.name.contains("a")):
-        print("{}: {}".format(result.id, result.name))
+    # Query the database
+    state = session.query(State).filter_by(name=state_name).first()
 
-        session.close()
+    # Print the result or Not found
+    if state is not None:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
