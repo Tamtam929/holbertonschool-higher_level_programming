f0a6deae536d7a4524e8b70566b7be82d9effbb2
Tamtam929 <tamtam929@gmail.com>
2023-02-28 17:32:51 -0800

    pycodestyle

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
 # Connecting to a MySQL database
 db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                    passwd=argv[2], db=argv[3])
+                     passwd=argv[2], db=argv[3])
 
 # Getting a Cursor in MySQL Python
 cur = db.cursor()
