cac93e36cc090c5d9458aec3ad0bb305c8f527a1
Tamtam929 <tamtam929@gmail.com>
2023-03-09 14:58:25 -0800

    hope this works

--- 0x0C-python-network_1/5-hbtn_header.py
+++ 0x0C-python-network_1/5-hbtn_header.py
@@ -1,1 +1,9 @@
-1
+#!/usr/bin/python3
+"""get X-Request-Id header and display it"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    res = requests.get(sys.argv[1])
+    print(res.headers.get('X-Request-Id'))
