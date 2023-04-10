74bee0358d7c152581bf3856eb23822207781c81
Tamtam929 <tamtam929@gmail.com>
2023-04-10 10:00:31 -0700

    please pass

--- 0x00-javascript-warm_up/3-value_argument.js
+++ 0x00-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,10 @@
-1
+#!/usr/bin/node
+// script that prints the first argument passed to it
+const { argv } = require('process');
+
+ if (argv[2] === undefined) {
+   console.log('No argument');
+   } else {
+     console.log(argv[2]);
+     }
+
