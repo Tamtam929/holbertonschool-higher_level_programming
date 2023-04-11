d99156f513b3ef83df2cc34df261394b3583a2e7
Tamtam929 <tamtam929@gmail.com>
2023-04-11 12:28:21 -0700

    semistandard

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -3,11 +3,10 @@
 const { argv } = require('process');
 const argc = argv.length - 2;
 
- if (argc === 0) {
-   console.log('No argument');
-   } else if (argc === 1) {
-     console.log('Argument found');
-     } else if (argc > 1) {
-       console.log('Arguments found');
-     }
-
+if (argc === 0) {
+  console.log('No argument');
+} else if (argc === 1) {
+  console.log('Argument found');
+} else if (argc > 1) {
+  console.log('Arguments found');
+}
--- 0x00-javascript-warm_up/5-to_integer.js
+++ 0x00-javascript-warm_up/5-to_integer.js
@@ -2,10 +2,9 @@
 // script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer
 const { argv } = require('process');
 
- const num = parseInt(argv[2]);
- if (isNaN(num)) {
-   console.log('Not a number');
-   } else {
-     console.log(`My number: ${argv[2]}`);
-     }
-
+const num = parseInt(argv[2]);
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${argv[2]}`);
+}
