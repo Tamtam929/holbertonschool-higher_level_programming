81451b659c6fe601c599fd0b8729c427ebe24dd3
TammyJunk <Tamtam929@gmail.com>
2023-04-16 16:59:08 +0000

    semistandard

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -2,12 +2,12 @@
 /*
  *Write a script that reads and prints the content of a file.
  */
- const request = require('request');
- const titleId = process.argv[2];
- const url = 'https://swapi-api.hbtn.io/api/films/';
- const urlId = `${url}${titleId}`;
- request(urlId, (error, response, body) => {
-   if (error) console.log(error);
-   const json = JSON.parse(body);
-   console.log(json.title);
- });
+const request = require('request');
+const titleId = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+const urlId = `${url}${titleId}`;
+request(urlId, (error, response, body) => {
+  if (error) console.log(error);
+  const json = JSON.parse(body);
+  console.log(json.title);
+});
