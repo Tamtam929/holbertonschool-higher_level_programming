de7bd43a9bb18f6620dea28b0551daaacd6da85a
TammyJunk <Tamtam929@gmail.com>
2023-04-16 18:15:04 +0000

    try again

--- 0x03-javascript-web_jquery/8-script.js
+++ 0x03-javascript-web_jquery/8-script.js
@@ -1,9 +1,5 @@
-$(document).ready(function(){
-	$.get('https://swapi-api.hbtn.io/api/films/?format=json', function(data)
-	{
-		$.each(data.results, function(index, value) {
-			$('#list_movies').append(`<li>${data.results[index].title}</li>`);
-			console.log(`${index}: ${data.results[index].title}`);
-		});
-	});
+$.get('https://swapi.co/api/films/?format=json', function (data) {
+  $.each(data.results, function (index, value) {
+    $('<li>' + value.title + '</li>').appendTo('UL#list_movies');
+  });
 });
--- 0x03-javascript-web_jquery/9-script.js
+++ 0x03-javascript-web_jquery/9-script.js
@@ -1,1 +1,3 @@
-1
+$.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+  $('DIV#hello').text(data.hello);
+});
